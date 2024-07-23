f9aa5ffb48c4373d135deee0e545996fa0126c63
joelquispe <joelsosayaquispe@gmail.com>
2024-07-23 00:39:27 -0500

    refactor: remove dependency innecesary

---
+++ src/test/java/com/yoe/openapiexample/presentation/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoe.openapiexample.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
